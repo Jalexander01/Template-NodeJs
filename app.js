d23f2bd2f3df16dc75cf6eafbfc00b6a77b4877d
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-05-06 08:06:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.use(bodyParser.urlencoded({extended:true}));//use body-parser to grab the da
 app.set('view engine', 'ejs');//to use ejs with express
 app.use(express.static("public"));//use static files like style.css and js
 
- 
 
+//get function will send information to the brower at loading
 app.get("/", function(req, res){
   // console.log(req);
   // res.sendFile();
@@ -17,12 +17,10 @@ app.get("/", function(req, res){
 
 })
 
+//post function will receive information from the browser to the server
 app.post("/", function(req, res){
-  // var numb1 = Number(req.body.num1);
-  // var numb2 = Number(req.body.num2);
-  // results = numb1 + numb2;
-
   res.send("The results of ");
+  res.redirect("/");//redirects to app.get function or another function
 })
 
 app.listen(3000, function(){
